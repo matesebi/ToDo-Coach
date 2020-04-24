6509ea98e9c746e5c373b491edcf5d967838f073
matesebi <matesebi96@gmail.com>
2020-04-25 00:15:19 +0200

    Add generated models

---
+++ app/src/main/kotlin/hu/aut/bme/matesebi/todocoach/model/Section.kt
@@ -0,0 +1,8 @@
+package hu.aut.bme.matesebi.todocoach.model
+
+data class Section(
+    val id: Int? = null,
+    val projectId: Int? = null,
+    val order: Int? = null,
+    val name: String? = null
+)
---
+++ app/src/main/kotlin/hu/aut/bme/matesebi/todocoach/model/Task.kt
@@ -0,0 +1,16 @@
+package hu.aut.bme.matesebi.todocoach.model
+
+data class Task (
+    val id: Int? = null,
+    val projectId: Int? = null,
+    val sectionId: Int? = null,
+    val content: String? = null,
+    val completed: Boolean? = null,
+    val labelIds: List<Int>? = null,
+    val parent: Int? = null,
+    val order: Int? = null,
+    val priority: Int? = null,
+    val due: Due? = null,
+    val url: String? = null,
+    val commentCount: Int? = null
+)
