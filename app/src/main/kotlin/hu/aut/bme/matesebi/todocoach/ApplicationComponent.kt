748127b400c75762484cbf2ffde6da9de043bd24
matesebi <matesebi96@gmail.com>
2020-04-25 16:50:58 +0200

    Add new modules to app component

--- app/src/main/kotlin/hu/aut/bme/matesebi/todocoach/ApplicationComponent.kt
+++ app/src/main/kotlin/hu/aut/bme/matesebi/todocoach/ApplicationComponent.kt
@@ -1,15 +1,23 @@
 package hu.aut.bme.matesebi.todocoach
 
 import dagger.Component
-import dagger.Subcomponent
+import hu.aut.bme.matesebi.todocoach.db.DBModule
 import hu.aut.bme.matesebi.todocoach.interactor.InteractorModule
+import hu.aut.bme.matesebi.todocoach.network.NetworkModule
 import hu.aut.bme.matesebi.todocoach.ui.UIModule
 import hu.aut.bme.matesebi.todocoach.ui.detail.ItemDetailActivity
 import hu.aut.bme.matesebi.todocoach.ui.list.ItemListActivity
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [UIModule::class, InteractorModule::class])
+@Component(
+    modules = [
+        NetworkModule::class,
+        DBModule::class,
+        UIModule::class,
+        InteractorModule::class
+    ]
+)
 interface ApplicationComponent {
     fun inject(itemListActivity: ItemListActivity)
     fun inject(itemDetailActivity: ItemDetailActivity)
