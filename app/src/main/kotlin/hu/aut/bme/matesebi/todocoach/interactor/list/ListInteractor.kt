b12178826e0be750ed59cd281190d793bf023299
matesebi <matesebi96@gmail.com>
2020-04-25 16:59:35 +0200

    Inject api and dao to interactors

--- app/src/main/kotlin/hu/aut/bme/matesebi/todocoach/interactor/list/ListInteractor.kt
+++ app/src/main/kotlin/hu/aut/bme/matesebi/todocoach/interactor/list/ListInteractor.kt
@@ -1,8 +1,11 @@
 package hu.aut.bme.matesebi.todocoach.interactor.list
 
+import hu.aut.bme.matesebi.todocoach.db.TodoDao
 import hu.aut.bme.matesebi.todocoach.model.DummyContent
+import hu.aut.bme.matesebi.todocoach.network.TodoApi
+import javax.inject.Inject
 
-class ListInteractor {
+class ListInteractor @Inject constructor(todoApi: TodoApi, todoDao: TodoDao) {
     fun getItems(): List<DummyContent> {
         return emptyList()
     }
